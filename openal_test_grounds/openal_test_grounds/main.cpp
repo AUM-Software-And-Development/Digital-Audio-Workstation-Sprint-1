7fa556d68ee289fb428ab34b3daededb0f9173f4
Eric Dee <eric_dee@outlook.com>
2022-02-08 02:05:24 -0700

    Changed dsp_utils directory location

--- openal_test_grounds/openal_test_grounds/main.cpp
+++ openal_test_grounds/openal_test_grounds/main.cpp
@@ -7,7 +7,7 @@ int main()
 {
 	ALAddressBlock* global_AL_address = ALAddressBlock::get();
 
-	uint32_t testing_vectors = SampleCache::get()->add_to_vector("C:\\development\\open_al-gl_digital_audio_workstation\\dsp_utils\\dsp_utils\\sine.wav");
+	uint32_t testing_vectors = SampleCache::get()->add_to_vector("C:\\development\\open_al-gl_digital_audio_workstation\\dsp_utils\\sine.wav");
 
 	PlaybackBuffer main_buffer;
 
