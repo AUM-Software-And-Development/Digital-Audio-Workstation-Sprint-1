c54452ace794964a86f6c7334de72212f50ffe21
Eric Dee <eric_dee@outlook.com>
2022-02-11 22:22:47 -0700

    Added base logging

--- opengl_test_grounds/opengl_test_grounds/src/quick_look/entry_point.h
+++ opengl_test_grounds/opengl_test_grounds/src/quick_look/entry_point.h
@@ -7,7 +7,34 @@
 inline int quick_look_main(ContextFactory* contextFactory, int argc, char** argv)
 {
 	GLLogger glLogger;
-	glLogger.get_core_logger()->info("Log opened.");
+
+// Intentionally visible regardless of build mode
+	/* Core */
+#define GL_LOG_NOTIFY_USING_INFO(...)            glLogger.get_core_logger()->info(__VA_ARGS__)
+#define GL_LOG_NOTIFY_USING_TRACE(...)           glLogger.get_core_logger()->trace(__VA_ARGS__)
+#define GL_LOG_NOTIFY_USING_WARN(...)            glLogger.get_core_logger()->warn(__VA_ARGS__)
+#define GL_LOG_NOTIFY_USING_ERROR(...)           glLogger.get_core_logger()->error(__VA_ARGS__)
+#define GL_LOG_NOTIFY_USING_CRITICAL(...)        glLogger.get_core_logger()->critical(__VA_ARGS__)
+    /* Client */
+#define GL_LOG_NOTIFY_CLIENT_USING_INFO(...)     glLogger.get_client_logger()->info(__VA_ARGS__)
+#define GL_LOG_NOTIFY_CLIENT_USING_TRACE(...)    glLogger.get_client_logger()->trace(__VA_ARGS__)
+#define GL_LOG_NOTIFY_CLIENT_USING_WARN(...)     glLogger.get_client_logger()->warn(__VA_ARGS__)
+#define GL_LOG_NOTIFY_CLIENT_USING_ERROR(...)    glLogger.get_client_logger()->error(__VA_ARGS__)
+#define GL_LOG_NOTIFY_CLIENT_USING_CRITICAL(...) glLogger.get_client_logger()->critical(__VA_ARGS__)
+/*_
+  */printf("\n");
+	GL_LOG_NOTIFY_USING_INFO                     ("This is an information example.");
+    GL_LOG_NOTIFY_USING_TRACE                    ("This is a trace example.");
+    GL_LOG_NOTIFY_USING_WARN                     ("This is a warning example.");
+    GL_LOG_NOTIFY_USING_ERROR                    ("This is an error example.");
+    GL_LOG_NOTIFY_USING_CRITICAL                 ("This is a critical example.");
+	printf("\n\n");
+	GL_LOG_NOTIFY_CLIENT_USING_INFO              ("This is a client information example.");
+	GL_LOG_NOTIFY_CLIENT_USING_TRACE             ("This is a client trace example.");
+	GL_LOG_NOTIFY_CLIENT_USING_WARN              ("This is a client warning example.");
+	GL_LOG_NOTIFY_CLIENT_USING_ERROR             ("This is a client error example.");
+	GL_LOG_NOTIFY_CLIENT_USING_CRITICAL          ("This is a client critical example.");
+	
 
 	printf("Starting int main from entry point of src/quick_look");
 	auto quickLookSandbox = contextFactory->get_and_build();
@@ -16,7 +43,6 @@ inline int quick_look_main(ContextFactory* contextFactory, int argc, char** argv
 
 	return 0;
 }
-
 #else
 #error Quick look is only buildable on Windows.
 
